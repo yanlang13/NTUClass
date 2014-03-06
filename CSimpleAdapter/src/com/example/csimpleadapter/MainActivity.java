8f690a9646eb0cc817ce173619972d9cfa5d64d1
yanlang13 <yanlang13@gmail.com>
2014-03-06 21:41:18 +0800

    點擊List產生反應
    listView.setOnItemClickListener(new OnItemClickListener(){
    @Override
    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
    long arg3) {
    Log.e("test", ""+arg2);
    
    }
    
    });

--- CSimpleAdapter/src/com/example/csimpleadapter/MainActivity.java
+++ CSimpleAdapter/src/com/example/csimpleadapter/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.csimpleadapter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,7 +23,18 @@ public class MainActivity extends Activity {
 
 		listView.setAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, data));
-
+		
+		//點擊item的反應
+		listView.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Log.e("test", ""+arg2);
+				
+			}
+			
+		});
+		
 	}
 
 	@Override
