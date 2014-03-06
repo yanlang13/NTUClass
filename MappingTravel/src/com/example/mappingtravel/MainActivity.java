9f4fb778c511e8af442ee0681f552959d65b8603
yanlang13 <yanlang13@gmail.com>
2014-03-06 20:34:14 +0800

    NEW

--- MappingTravel/src/com/example/mappingtravel/MainActivity.java
+++ MappingTravel/src/com/example/mappingtravel/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-//tt
 public class MainActivity extends Activity {
 
 	@Override
