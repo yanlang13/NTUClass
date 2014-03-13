f07a0d0b31ae5ea439e3ffa46b5c02cbdda07541
yanlang13 <yanlang13@gmail.com>
2014-03-13 09:52:59 +0800

    tt

--- Calculator/src/com/example/calculator/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.calculator;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-
-}
--- TestFirst/src/com/example/testfirst/MainActivity.java
+++ TestFirst/src/com/example/testfirst/MainActivity.java
@@ -1,29 +1,22 @@
 package com.example.testfirst;
 
-
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-	private Button button;
+	private static final int REQUEST_CODE = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		button = (Button) findViewById(R.id.button1);
-		//內建
+
+		// 內建
 		// if (savedInstanceState == null) {
 		// getSupportFragmentManager().beginTransaction()
 		// .add(R.id.container, new PlaceholderFragment()).commit();
@@ -36,8 +29,7 @@ public class MainActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-		
-	
+
 	}
 
 	@Override
@@ -51,20 +43,26 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
 
-	
-	public void ButtonOnClick(View view){
-		Intent intent = new Intent(this, SecondActivity.class);
-		Bundle bundle = new Bundle();
-		bundle.putString("Name", "Wang");
-		bundle.putDouble("Age", 25.0);
-		intent.putExtras(bundle);
-		startActivity(intent);
-		
+	public void ButtonOnClick(View view) {
+		Intent i = new Intent(this, SecondActivity.class);
+		i.putExtra("Value1", "This value one for ActivityTwo ");
+		i.putExtra("Value2", "This value two ActivityTwo");
+		// set the request code to any code you like,
+		// you can identify the callback via this code
+		startActivityForResult(i, REQUEST_CODE);
 	}
-	//內建
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+			if (data.hasExtra("returnKey1")) {
+				Toast.makeText(this, data.getExtras().getString("returnKey1"),
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	// 內建
 	// /**
 	// * A placeholder fragment containing a simple view.
 	// */
