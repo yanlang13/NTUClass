9870804ed380b8be6f09503aadc92d3ab5f5d96f
yanlang13 <yanlang13@gmail.com>
2014-03-17 21:06:21 +0800

    寫入檔案到SD卡
    Manifest 權限也需要新增

--- takephoto/src/com/example/takephoto/MainActivity.java
+++ takephoto/src/com/example/takephoto/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.takephoto;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
@@ -45,18 +52,49 @@ public class MainActivity extends ActionBarActivity {
 	// 第一個值確認是誰回來
 	protected void onActivityResult(int requestcode, int resultcode, Intent data) {
 		super.onActivityResult(requestcode, resultcode, data);
+		// 確認回來的內容
 		if (requestcode == REQUEST_CODE_TAKE_PHOTO) {
+			// result_ok有東西回來
 			if (resultcode == RESULT_OK) {
+				// Bitmap (指的是圖)
 				Bitmap image = (Bitmap) data.getExtras().get("data");
+				save(image);
 				imageView.setImageBitmap(image);
 			}
 		}
 	}
 
-	// 確認回來的內容
-	// result_ok有東西回來
+	// 寫入檔案到SD卡
+	private void save(Bitmap bitmap) {
+		// 抓到SD卡 type也是定義在env中，這邊抓到的是資料夾(可能不存在)
+		File imageDir = Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-	// Bitmap (指的是圖)
+		// 確認資料夾狀態
+		if (imageDir.exists()) {
+			imageDir.mkdir();
+		}
+
+		// 做出檔案
+		File imageFile = new File(imageDir, "photo.png");
+
+		// 寫檔案，也是串流的概念，bufferOutput會比fileoutput快
+		try {
+			BufferedOutputStream bos = new BufferedOutputStream(
+					new FileOutputStream(imageFile));
+			// compress轉換，format 格式，quality 0-100,??)
+			bitmap.compress(Bitmap.CompressFormat.PNG, 90, bos);
+			Log.e("debug", "stroagepath= " + imageFile.getAbsolutePath());
+			bos.flush();
+			bos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
