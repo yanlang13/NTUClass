7ebe1865fe29074750b86be4d295dceda57d3e01
yanlang13 <yanlang13@gmail.com>
2014-03-21 11:18:07 +0800

    Gmaker

--- MapMyLife/gen/com/example/mapmylife/R.java
+++ MapMyLife/gen/com/example/mapmylife/R.java
@@ -233,7 +233,7 @@ containing a value of this type.
         public static final int action_share=0x7f050009;
         public static final int container=0x7f050006;
         public static final int hybrid=0x7f050004;
-        public static final int map=0x7f050005;
+        public static final int mmap=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
--- MapMyLife/src/com/example/mapmylife/GmapActivity.java
+++ MapMyLife/src/com/example/mapmylife/GmapActivity.java
@@ -1,11 +1,51 @@
 package com.example.mapmylife;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import android.os.Bundle;
 
 public class GmapActivity extends ActivityWithMenu {
+
+	private GoogleMap mMap;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_gmap);
 		setActionBarColor();
-	}
-}
+		setContentView(R.layout.activity_gmap);
+		setUpMapIfNeeded();
+	}// end of onCreate();
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// 使用activity時，supportMapFragment，和getSupportFragmentManager都要將support拿掉(appv4)
+			mMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.mmap)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+				mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+			}
+		}
+	}// end of setUpMapIfNeeded();
+
+	private void setUpMap() {
+		/**
+		 * This is where we can add markers or lines, add listeners or move the
+		 * camera. This should only be called once and when we are sure that
+		 * mMap is not null.
+		 */
+		MarkerOptions mk = new MarkerOptions();
+		LatLng position = new LatLng(10, 10);
+		mk.position(position);
+		mk.title("maker1");
+		mMap.addMarker(mk);
+		mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title(
+				"Maker"));
+	}// end of setUpMap();
+
+}// end of class GmapActiviy
+
