e99c1afc65de3ab1a69d3dad0662fd2822476b33
yanlang13 <yanlang13@gmail.com>
2014-03-24 20:39:19 +0800

    exampleList基本地圖

--- ExampleList/src/com/example/examplelist/Example1Activity.java
+++ ExampleList/src/com/example/examplelist/Example1Activity.java
@@ -1,11 +1,43 @@
 package com.example.examplelist;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class Example1Activity extends Activity {
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.example1);
-	}// end of onCreate()
+	private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.example1);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+        	mMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.mMap)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
 }
--- maps/src/com/example/mapdemo/MainActivity.java
+++ maps/src/com/example/mapdemo/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.mapdemo;
 
 import android.app.ListActivity;
@@ -28,21 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
-/**
- * The main activity of the API library demo gallery.
- * <p>
- * The main layout lists the demonstrated features, with buttons to launch them.
- */
 public final class MainActivity extends ListActivity {
 
-    /**
-     * A custom array adapter that shows a {@link FeatureView} containing details about the demo.
-     */
     private static class CustomArrayAdapter extends ArrayAdapter<DemoDetails> {
-
-        /**
-         * @param demos An array containing the details of the demos to be displayed.
-         */
         public CustomArrayAdapter(Context context, DemoDetails[] demos) {
             super(context, R.layout.feature, R.id.title, demos);
         }
