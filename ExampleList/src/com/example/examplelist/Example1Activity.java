2bdba15b1cfdb848e320c4ef4410d4384c53bcf0
yanlang13 <yanlang13@gmail.com>
2014-03-24 22:02:06 +0800

    AlertDialog
    Builder aa = ....

--- ExampleList/src/com/example/examplelist/Example1Activity.java
+++ ExampleList/src/com/example/examplelist/Example1Activity.java
@@ -5,56 +5,55 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.animation.AnimatorSet.Builder;
 import android.app.Activity;
 import android.os.Bundle;
+import android.sax.EndElementListener;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Example1Activity extends Activity {
-	private GoogleMap mMap;
-	private Button buttonMapType;
+	private GoogleMap mMap; 
+	private Button buttonMapType; //change map type
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.example1);
 		setUpMapIfNeeded();
-
-		buttonMapType = (Button) findViewById(R.id.buttonMapType);
-		buttonMapType.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Log.d("debug", "Type Click");
-			}
-		});// end of buttonMapType.setOnClickListener
-
 	}// end of onCcreate{}
-
+	
+	//change map type
+	public void ClickMapType(View view){
+		Log.d("debuf", "click map type");
+		showAlertDialog();
+	}//End of ClickMapType()
+	
+	//select map type
+	public void showAlertDialog(){
+		
+	}//End of showAlertDialog()
+	
 	@Override
 	protected void onResume() {
 		super.onResume();
 		setUpMapIfNeeded();
 	}// end of onResume()
-
+	
 	private void setUpMapIfNeeded() { // call from onResume()
-		// Do a null check to confirm that we have not already instantiated the
-		// map.
 		if (mMap == null) {
-			// Try to obtain the map from the SupportMapFragment.
 			mMap = ((MapFragment) getFragmentManager().findFragmentById(
 					R.id.mMap)).getMap();
-			// Check if we were successful in obtaining the map.
 			if (mMap != null) {
 				setUpMap();
 			}
 		}
 	}// end of setUpMapIfNeeded()
-
+	
 	private void setUpMap() { // call from setUpMapIfNeeded()
 		mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title(
 				"Marker"));
 	}// end of setUpMap()
-
 }
