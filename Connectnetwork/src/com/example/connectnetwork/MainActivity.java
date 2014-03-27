75ad65e892b4b27dae28cb20ae2930eeab63319a
yanlang13 <yanlang13@gmail.com>
2014-03-27 20:12:24 +0800

    取得網頁原始碼
    注意androidManifest也需要修改

--- Connectnetwork/src/com/example/connectnetwork/MainActivity.java
+++ Connectnetwork/src/com/example/connectnetwork/MainActivity.java
@@ -1,70 +1,84 @@
 package com.example.connectnetwork;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView textView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.textView1);
+
 	}
-	//先連接網路
-	public void fetch(View view){
-		String urlString = "http://yahoo.com";
-		//1. java內建
-		try {
-			//確認url的正確性
-			URL url = new URL(urlString);
-			//開啟連線，但也會有例外
-			URLConnection connection = url.openConnection();
-			//讀網頁(同讀檔的概念)
-			BufferedReader buffer = new BufferedReader(new InputStreamReader());
-			
-			//一次讀一行，讀到檔案為空
-			
-			//result儲存讀取的結果，再放到textview
-			
-		} catch (Exception e) { // url的正確性
-			// TODO: handle exception
-		}catch (){
-			
-		}
-		//2.aptch提供
+
+	// 連接網路後，fetch必須放到另外一個thread，這樣才不會crash (android 3.0以後的限制)
+	public void fetch(View view) {
+
+		// android 獨特的方式:把thread分解成三個部分(在task執行前、中、後)
+		//第二個可用來監控進度，第三個Type是指回傳的值
+		AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+			String urlStr = "http://yahoo.com";
+			@Override
+			protected void onPreExecute() {
+			}
+			@Override
+			protected String doInBackground(Void... params) {
+				try {
+					// 確認網址
+					URL url = new URL(urlStr);
+					URLConnection connection = url.openConnection();
+
+					// 讀網址內容(原始碼)
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(connection.getInputStream()));
+
+					// 用StringBuilder加快速度
+					StringBuilder stringBuilder = new StringBuilder();
+					String line;
+					while ((line = buffer.readLine()) != null) {
+						stringBuilder.append(line);
+					}
+					return stringBuilder.toString();
+					// 網址錯誤的exception
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					// 網頁問題的exception
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}// end of try
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				textView.setText(result);
+			}
+		};
+		task.execute();
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+}
