1c0c93a8bd6f0e4cc2b8710bbe7b981fddfd2427
yanlang13 <yanlang13@gmail.com>
2014-04-03 15:23:16 +0800

    簡單的setOnItemsListener，有return的方法

--- ExampleList/gen/com/example/examplelist/R.java
+++ ExampleList/gen/com/example/examplelist/R.java
@@ -9,6 +9,7 @@ package com.example.examplelist;
 
 public final class R {
     public static final class array {
+        public static final int camera_dialog_list=0x7f090001;
         public static final int map_type=0x7f090000;
     }
     public static final class attr {
@@ -225,53 +226,56 @@ containing a value of this type.
         public static final int ic_plusone_tall_off_client=0x7f02001c;
     }
     public static final class id {
-        public static final int CameraPosition=0x7f050018;
-        public static final int action_settings=0x7f05001c;
+        public static final int CameraPosition=0x7f05001b;
+        public static final int action_settings=0x7f05001f;
         public static final int camera_list=0x7f050008;
+        public static final int camera_list_delete=0x7f05000e;
+        public static final int camera_list_modify=0x7f05000d;
+        public static final int camera_list_title=0x7f05000c;
         public static final int camera_position=0x7f050006;
-        public static final int description=0x7f05001b;
-        public static final int dialog_description=0x7f050014;
-        public static final int dialog_name=0x7f050012;
-        public static final int dialog_title=0x7f050010;
+        public static final int description=0x7f05001e;
+        public static final int dialog_description=0x7f050017;
+        public static final int dialog_name=0x7f050015;
+        public static final int dialog_title=0x7f050013;
         public static final int gMap=0x7f050007;
         public static final int hybrid=0x7f050004;
-        public static final int listView1=0x7f050016;
-        public static final int list_desc=0x7f05000f;
-        public static final int list_lat=0x7f05000d;
-        public static final int list_lng=0x7f05000e;
-        public static final int list_name=0x7f05000c;
-        public static final int mMap=0x7f050017;
+        public static final int listView1=0x7f050019;
+        public static final int list_desc=0x7f050012;
+        public static final int list_lat=0x7f050010;
+        public static final int list_lng=0x7f050011;
+        public static final int list_name=0x7f05000f;
+        public static final int mMap=0x7f05001a;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int press_position=0x7f050005;
         public static final int satellite=0x7f050002;
         public static final int save_camera=0x7f050009;
-        public static final int spMapType=0x7f050019;
+        public static final int spMapType=0x7f05001c;
         public static final int terrain=0x7f050003;
-        public static final int textView1=0x7f050011;
-        public static final int textView11=0x7f050015;
-        public static final int textView2=0x7f050013;
+        public static final int textView1=0x7f050014;
+        public static final int textView11=0x7f050018;
+        public static final int textView2=0x7f050016;
         public static final int tilt_less=0x7f05000b;
         public static final int tilt_more=0x7f05000a;
-        public static final int title=0x7f05001a;
+        public static final int title=0x7f05001d;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
         public static final int camera=0x7f030000;
-        public static final int camera_list_view=0x7f030001;
-        public static final int camera_save_dialog=0x7f030002;
-        public static final int cameralist=0x7f030003;
-        public static final int example1=0x7f030004;
-        public static final int feature=0x7f030005;
-        public static final int main=0x7f030006;
+        public static final int camera_list_dialog=0x7f030001;
+        public static final int camera_list_view=0x7f030002;
+        public static final int camera_save_dialog=0x7f030003;
+        public static final int cameralist=0x7f030004;
+        public static final int example1=0x7f030005;
+        public static final int feature=0x7f030006;
+        public static final int main=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f0b0000;
     }
     public static final class string {
-        public static final int Camera_List=0x7f06002c;
         public static final int action_settings=0x7f06001b;
         public static final int app_name=0x7f06001a;
         /**   Title for notification shown when GooglePlayServices needs to be
@@ -299,6 +303,7 @@ containing a value of this type.
         public static final int auth_client_using_bad_version_title=0x7f060014;
         public static final int camera_description=0x7f06001e;
         public static final int camera_label=0x7f06001c;
+        public static final int camera_list=0x7f06002c;
         public static final int camera_list_label=0x7f06001d;
         /**  Button in confirmation dialog to enable Google Play services.  Clicking it
         will direct user to application settings of Google Play services where they
@@ -383,7 +388,7 @@ containing a value of this type.
         public static final int normal=0x7f060029;
         public static final int press_position=0x7f06001f;
         public static final int satellite=0x7f06002a;
-        public static final int save_Camera=0x7f06002d;
+        public static final int save_camera=0x7f06002d;
         public static final int terrain=0x7f06002b;
         public static final int up_arrow=0x7f06002e;
     }
--- ExampleList/src/com/example/examplelist/CameraListActivity.java
+++ ExampleList/src/com/example/examplelist/CameraListActivity.java
@@ -12,8 +12,13 @@ import com.parse.ParseQuery;
 
 import android.R.integer;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.StaticLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
@@ -43,27 +48,49 @@ public class CameraListActivity extends Activity {
 		arrayListLat = new ArrayList<Double>();
 		arrayListLng = new ArrayList<Double>();
 		nF = new DecimalFormat("#.###");
-
 		progressDialog = new ProgressDialog(this);
+
 		progressDialog.show();
-
 		getDataFromParse();
+		setUpAllListener();
 
-		//長按的listener
-		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-		});// end of listView.setOnItemLongClickListener
 	}// end of onCreate
 
-	/*
-	 * 取得parse上的資料
-	 */
-	private void getDataFromParse() {
+	// 所有的listener
+	private void setUpAllListener() { // call from onCreate
+		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				setAlertDialog().show();
+				return false;
+			}//end of onItemLongClick
+		});// end of listView.setOnItemLongClickListener
+	}// end of setUpAllListener()
+	
+	private AlertDialog setAlertDialog(){// call from setUpAllListener()
+		AlertDialog.Builder aDialogBuilder = new AlertDialog.Builder(
+				CameraListActivity.this);
+//		LayoutInflater inflater = CameraListActivity.this.getLayoutInflater();
+//		View dialogView = inflater.inflate(R.layout.camera_list_dialog, null);
+//		aDialogBuilder.setView(dialogView);
+		
+		final String editSelect [] = new String[]{"Modify", "Delete"};
+		aDialogBuilder.setItems(editSelect , new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(CameraListActivity.this, editSelect[which], Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		return aDialogBuilder.create();
+		
+	}// end of setAlertDialog()
+	
+	
+	// {{getDataFromParse()取得parse資料，setUpListView()放到lsit
+	// 取得parse上的資料
+	private void getDataFromParse() { // call from onCreate
 		// ParseQuery 使用 subclass CameraSaveParse
 		// getQuery Creates a new query for the given ParseObject subclass type.
 		ParseQuery<CameraSaveParse> query = ParseQuery
@@ -78,32 +105,10 @@ public class CameraListActivity extends Activity {
 						arrayListDesc.add(csp.getDesc());
 						ParseGeoPoint pgp = new ParseGeoPoint();
 						pgp = csp.getParseGeoPoint("ParseGeoPoint");
-
 						arrayListLat.add(pgp.getLatitude());
 						arrayListLng.add(pgp.getLongitude());
-
 					}// end of for each
-					ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
-					for (int i = 0; i < arrayListName.size(); i++) {
-						HashMap<String, Object> item = new HashMap<>();
-						item.put("Name", arrayListName.get(i));
-						item.put("Desc", arrayListDesc.get(i));
-						item.put("Lat", nF.format(arrayListLat.get(i)));
-						item.put("Lng", nF.format(arrayListLng.get(i)));
-						list.add(item);
-					}
-
-					// 新增SimpleAdapter
-					String[] from = { "Name", "Desc", "Lat", "Lng" };
-					int[] to = { R.id.list_name, R.id.list_desc, R.id.list_lat,
-							R.id.list_lng };
-
-					SimpleAdapter simpleAdapter;
-					simpleAdapter = new SimpleAdapter(getApplication(), list,
-							R.layout.camera_list_view, from, to);
-
-					// ListActivity設定adapter
-					listView.setAdapter(simpleAdapter);
+					setUpListView();
 				} else {
 					Toast.makeText(getApplication(), e.toString(),
 							Toast.LENGTH_LONG).show();
@@ -114,5 +119,30 @@ public class CameraListActivity extends Activity {
 		});// end of query.findInBackground
 	}// end of getDataFromParse
 
+	// 將parse資料放到listView
+	private void setUpListView() { // call from getDataFromParse()
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		for (int i = 0; i < arrayListName.size(); i++) {
+			HashMap<String, Object> item = new HashMap<>();
+			item.put("Name", "Name: " + arrayListName.get(i));
+			item.put("Desc", "Description: \n" + arrayListDesc.get(i));
+			item.put("Lat", "Latitude:" + nF.format(arrayListLat.get(i)));
+			item.put("Lng", "Longtitude:" + nF.format(arrayListLng.get(i)));
+			list.add(item);
+		}
+
+		// 新增SimpleAdapter
+		String[] from = { "Name", "Desc", "Lat", "Lng" };
+		int[] to = { R.id.list_name, R.id.list_desc, R.id.list_lat,
+				R.id.list_lng };
+
+		SimpleAdapter simpleAdapter;
+		simpleAdapter = new SimpleAdapter(getApplication(), list,
+				R.layout.camera_list_view, from, to);
+
+		// ListActivity設定adapter
+		listView.setAdapter(simpleAdapter);
+	}// end of setListView
+		// }}
 }// end of CameraListActivity
 
